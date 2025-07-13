a4d879785522d5ba003128c7d280bebdfdf84fef
Ayush <ayushshivhare51@gmail.com>
2025-07-13 11:57:20 +0530

    feat: drizzle setup and login with goole

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,19 @@
 import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "./db/index";
+import * as schema from "./db/schema";
+import { nextCookies } from "better-auth/next-js";
 
-export const auth = betterAuth({});
+export const auth = betterAuth({
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: schema,
+  }),
+  plugins: [nextCookies()],
+});
